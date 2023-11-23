ced50cc1e1c15a14c8b471360570c24e580cf022
Fatul_gb62 <Fatul_gb62@itstep.edu.az>
2023-11-23 21:45:34 +0400

    creating basis for array of objects homework

---
+++ ArrayOfObjectsHomework/Item.cpp
@@ -0,0 +1,38 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include "Item.h"
+#include <iostream>
+
+Item::Item(const char* a_name, int a_price)
+{
+    m_name = new char[strlen(a_name) + 1];
+    strcpy(m_name, a_name);
+    m_price = a_price;
+}
+
+Item::~Item()
+{
+    delete[] m_name;
+}
+
+char* Item::get_name()
+{
+    return m_name;
+}
+
+int Item::get_price()
+{
+    return m_price;
+}
+
+void Item::set_name(const char* a_name)
+{
+    delete[] m_name;
+    m_name = new char[strlen(a_name) + 1];
+    strcpy(m_name, a_name);
+}
+
+void Item::set_price(int a_price)
+{
+    m_price = a_price;
+}
+
---
+++ ArrayOfObjectsHomework/Item.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+class Item
+{
+public:
+    Item(const char* a_name, int a_price);
+
+    ~Item();
+
+    char* get_name();
+
+    int get_price();
+
+    void set_name(const char* a_name);
+
+    void set_price(int a_price);
+
+    
+private:
+    char* m_name;
+    int m_price;
+};
