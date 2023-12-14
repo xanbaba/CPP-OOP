c5ab8a4fa956dcfb5b83fa5e8f2d1e33d4f9fea4
XanbabaF <xanbabaf@gmail.com>
2023-12-14 17:25:57 +0400

    default markup of the class SortedRandomArray

--- FunctorHW/SortedRandomArray.cpp
+++ FunctorHW/SortedRandomArray.cpp
@@ -1,1 +1,31 @@
 ﻿#include "SortedRandomArray.h"
+
+SortedRandomArray::SortedRandomArray(int a_numbers_count)
+{
+    for (int i = 0; i < a_numbers_count; ++i)
+    {
+        m_array.append(m_range(m_engine));
+    }
+}
+
+SortedRandomArray::SortedRandomArray(const SortedRandomArray& a_original)
+{
+    m_array = a_original.m_array;
+}
+
+SortedRandomArray::SortedRandomArray(SortedRandomArray&& a_original) noexcept
+{
+    m_array = std::move(a_original.m_array);
+}
+
+SortedRandomArray& SortedRandomArray::operator=(const SortedRandomArray& a_original)
+{
+    m_array = a_original.m_array;
+    return *this;
+}
+
+SortedRandomArray& SortedRandomArray::operator=(SortedRandomArray&& a_original) noexcept
+= default;
+
+SortedRandomArray::~SortedRandomArray()
+= default;
--- FunctorHW/SortedRandomArray.h
+++ FunctorHW/SortedRandomArray.h
@@ -1,10 +1,22 @@
 ﻿#pragma once
+#include "Greater.h"
 #include "IntArray.h"
+#include "Less.h"
+#include <random>
 
 class SortedRandomArray
 {
 public:
-    SortedRandomArray(int numbers_count);
+    SortedRandomArray(int a_numbers_count);
+    SortedRandomArray(const SortedRandomArray& a_original);
+    SortedRandomArray(SortedRandomArray&& a_original) noexcept;
+    SortedRandomArray& operator=(const SortedRandomArray& a_original);
+    SortedRandomArray& operator=(SortedRandomArray&& a_original) noexcept;
+    ~SortedRandomArray();
+    void Sort(Greater a_greater);
+    void Sort(Less a_less);
 private:
-    IntArray array;
+    IntArray m_array;
+    inline static std::default_random_engine m_engine{static_cast<unsigned int>(time(nullptr))};
+    inline static std::uniform_int_distribution m_range{0, 99};
 };
