0fb4ff7ecc59e5f1fa1e2fbbbc8ec1202eb78e31
XanbabaF <xanbabaf@gmail.com>
2023-12-19 21:22:54 +0400

    Template Class HW

--- TemplateClassHW/TemplateClassHW.cpp
+++ TemplateClassHW/TemplateClassHW.cpp
@@ -1,10 +1,11 @@
-
+#include "Array.h"
 #include "Dictionary.h"
+#include <string>
 
 int main(int argc, char* argv[])
 {
     // while (true)
-    {
+    /*{
         {
             Dictionary<const char*, int> dictionary;
             dictionary["first"] = 10;
@@ -53,6 +54,30 @@ int main(int argc, char* argv[])
             Dictionary<std::string, int> dictionary4 = std::move(dictionary3);
             std::cout << dictionary4["four"] << '\n';
         }
+    }*/
+
+    // while (true)
+    {
+        Array<std::string> array1{2};
+        array1.Prepend("Hello ");
+        array1.Insert("World ", 1);
+        array1.Append("!!!");
+        Array<std::string> array2{array1};
+        std::cout << array1 << '\n';
+        array1.Append("I Love You!");
+        std::cout << array1 << '\n';
+        std::cout << array2 << '\n';
+        Array<std::string> array3{std::move(array1)};
+        std::cout << array3 << '\n';
+        std::cout << array1 << '\n';
+        Array<std::string> array4;
+        array4 = array3;
+        array3.Append("AA");
+        std::cout << array4 << '\n';
+        Array<std::string> array5;
+        array5 = std::move(array3);
+        std::cout << array3 << '\n';
+        std::cout << array5 << '\n';
     }
     return 0;
 }
