54348bd493e745a7928376015432a795bc5fef99
XanbabaF <xanbabaf@gmail.com>
2024-01-10 18:06:35 +0400

    linked list homework complete

--- LinkedList/LinkedList.h
+++ LinkedList/LinkedList.h
@@ -1,4 +1,5 @@
 ﻿#pragma once
+#include <iostream>
 
 template <typename T>
 class LinkedList
@@ -7,7 +8,10 @@ public:
     class Node
     {
     public:
-        Node(const T& AValue) :Value(AValue), Next(), Prev() {}
+        Node(const T& AValue) : Value(AValue), Next(), Prev()
+        {
+        }
+
         T Value;
         Node* Next;
         Node* Prev;
@@ -15,28 +19,112 @@ public:
 
     ~LinkedList()
     {
-
         Clear();
     }
 
     LinkedList() : MHead(), MTail(), MSize()
     {
     }
+    
+    LinkedList(const LinkedList& AOriginal)
+    {
+        auto CurrentNode = AOriginal.GetHeadNode();
+        MSize = AOriginal.MSize;
+        if (CurrentNode == nullptr)
+        {
+            MHead = MTail = nullptr;
+            return;
+        }
+        MHead = new Node{CurrentNode->Value};
+        auto PrevNode = MHead;
+        CurrentNode = CurrentNode->Next;
+        Node* LNode = MHead;
+        while (CurrentNode != nullptr)
+        {
+            LNode = new Node{CurrentNode->Value};
+            LNode->Prev = PrevNode;
+            PrevNode->Next = LNode;
+            PrevNode = LNode;
+            CurrentNode = CurrentNode->Next;
+        }
+        MTail = LNode;
+    }
+    
+    LinkedList(LinkedList&& AOriginal) noexcept
+    {
+        MHead = AOriginal.MHead;
+        AOriginal.MHead = nullptr;
+        
+        MTail = AOriginal.MTail;
+        AOriginal.MTail = nullptr;
+        
+        MSize = AOriginal.MSize;
+        AOriginal.MSize = 0;
+        
+    }
+    LinkedList& operator=(const LinkedList& AOriginal)
+    {
+        if (this != &AOriginal)
+        {
+            this->~LinkedList();
+            auto CurrentNode = AOriginal.GetHeadNode();
+            MSize = AOriginal.MSize;
+            if (CurrentNode == nullptr)
+            {
+                MHead = MTail = nullptr;
+                return *this;
+            }
+            MHead = new Node{CurrentNode->Value};
+            auto PrevNode = MHead;
+            CurrentNode = CurrentNode->Next;
+            Node* LNode = MHead;
+            while (CurrentNode != nullptr)
+            {
+                LNode = new Node{CurrentNode->Value};
+                LNode->Prev = PrevNode;
+                PrevNode->Next = LNode;
+                PrevNode = LNode;
+                CurrentNode = CurrentNode->Next;
+            }
+            MTail = LNode;
+        }
 
-    void PushBack(const T& AElement)
+        return *this;
+    }
+    
+    LinkedList& operator=(LinkedList&& AOriginal) noexcept
+    {
+        if (this != &AOriginal)
+        {
+            this->~LinkedList();
+            
+            MHead = AOriginal.MHead;
+            AOriginal.MHead = nullptr;
+        
+            MTail = AOriginal.MTail;
+            AOriginal.MTail = nullptr;
+        
+            MSize = AOriginal.MSize;
+            AOriginal.MSize = 0;
+        }
+        return *this;
+    }
+
+    Node* PushBack(const T& AElement)
     {
         auto LNode = new Node{AElement};
         if (MSize == 0)
         {
             MHead = MTail = LNode;
             ++MSize;
-            return;
+            return LNode;
         }
 
         MTail->Next = LNode;
         LNode->Prev = MTail;
         MTail = LNode;
         ++MSize;
+        return LNode;
     }
 
     void PopFront()
@@ -68,12 +156,12 @@ public:
         MSize = 0;
     }
 
-    bool Empty()
+    bool IsEmpty() const
     {
         return MSize == 0;
     }
 
-    void InsertAfter(const T& AElement, const Node* APosition)
+    void InsertAfter(const T& AElement, Node* APosition)
     {
         auto LNode = new Node{AElement};
         auto CurrentNext = APosition->Next;
@@ -81,6 +169,46 @@ public:
         CurrentNext->Prev = LNode;
         LNode->Prev = APosition;
         LNode->Next = CurrentNext;
+        if (APosition == MTail)
+        {
+            MTail = LNode;
+        }
+    }
+
+    void Remove(const Node* APosition)
+    {
+        auto PrevNode = APosition->Prev;
+        auto NextNode = APosition->Next;
+        if (MHead == MTail)
+        {
+        }
+        else if (APosition == MHead)
+        {
+            MHead = NextNode;
+            NextNode->Prev = nullptr;
+        }
+        else if (APosition == MTail)
+        {
+            MTail = PrevNode;
+            PrevNode->Next = nullptr;
+        }
+        else
+        {
+            PrevNode->Next = NextNode;
+            NextNode->Prev = PrevNode;
+        }
+        --MSize;
+        delete APosition;
+    }
+
+    int Count() const
+    {
+        return MSize;
+    }
+
+    Node* GetHeadNode() const
+    {
+        return MHead;
     }
 
 private:
@@ -88,3 +216,16 @@ private:
     Node* MTail;
     int MSize;
 };
+
+
+template <typename T>
+std::ostream& operator<<(std::ostream& out, LinkedList<T>& linked_list)
+{
+    auto current_node = linked_list.GetHeadNode();
+    while (current_node != nullptr)
+    {
+        out << current_node->Value << '\n';
+        current_node = current_node->Next;
+    }
+    return out;
+}
--- LinkedList/main.cpp
+++ LinkedList/main.cpp
@@ -1,18 +1,117 @@
-
 #include <iostream>
 
 #include "LinkedList.h"
+
+// TEST CLASS TO TEST LIST WITH OBJECTS
+// --------------------------------------------------- //
+class User
+{
+public:
+    ~User()
+    {
+        delete[] data;
+    }
+
+    User()
+    {
+        data = new int[5];
+    }
+
+    User(const User& a_other)
+    {
+        data = new int[5];
+        memcpy(data, a_other.data, sizeof(int) * 5);
+    }
+
+    User& operator=(const User& a_other)
+    {
+        if (this != &a_other)
+        {
+            this->~User();
+            data = new int[5];
+            memcpy(data, a_other.data, sizeof(int) * 5);
+        }
+        return *this;
+    }
+
+    int* data;
+};
+
+// TEST CLASS TO TEST LIST WITH OBJECTS
+// --------------------------------------------------- //
+
+
+std::ostream& operator<<(std::ostream& out, User queue)
+{
+    out << 1;
+    return out;
+}
 
 int main(int argc, char* argv[])
 {
     while (true)
     {
-        LinkedList<int> LList;
-        LList.PushBack(10);
-        LList.PushBack(20); 
-        LList.PushBack(30);
+        {
+            LinkedList<int> LList;
+            auto el1 = LList.PushBack(10);
+            LList.Remove(el1);
+            auto el2 = LList.PushBack(20);
+            LList.PushBack(30);
+            LList.InsertAfter(40, el2);
+            LList.PushBack(50);
+            LList.PopBack();
+            LList.PopFront();
+            LList.PushBack(15);
+            LList.InsertAfter(25, LList.GetHeadNode());
+            std::cout << LList << '\n';
+            std::cout << LList.GetHeadNode()->Value << "\n\n";
 
-        std::cout << LList.GetHead() << '\n';
+            LinkedList<int> LListEmpty;
+
+            LinkedList<int> LList2{LList};
+            std::cout << LList2 << '\n';
+
+            LinkedList<int> LList3{LListEmpty};
+
+            LinkedList<int> LList4;
+            LList4 = LList2;
+            std::cout << LList4 << '\n';
+
+            LinkedList<int> LList5;
+            LList5 = LListEmpty;
+            std::cout << LList5 << '\n';
+        }
+        
+        {
+            LinkedList<User> LList;
+            auto el1 = LList.PushBack(User{});
+            LList.Remove(el1);
+            auto el2 = LList.PushBack(User{});
+            LList.PushBack(User{});
+            LList.InsertAfter(User{}, el2);
+            LList.PushBack(User{});
+            LList.PopBack();
+            LList.PopFront();
+            LList.PushBack(User{});
+            LList.InsertAfter(User{}, LList.GetHeadNode());
+            std::cout << LList << '\n';
+            std::cout << LList.GetHeadNode()->Value << "\n\n";
+
+            LinkedList<User> LListEmpty;
+
+            LinkedList<User> LList2{LList};
+            std::cout << LList2 << '\n';
+
+            LinkedList<User> LList3{LListEmpty};
+
+            LinkedList<User> LList4;
+            LList4 = LList2;
+            std::cout << LList4 << '\n';
+
+            LinkedList<User> LList5;
+            LList5 = LListEmpty;
+            std::cout << LList5 << '\n';
+        }
     }
     return 0;
 }
