57a6134537d8c1fc65edb01c545b982810738d44
XanbabaF <xanbabaf@gmail.com>
2024-01-20 13:33:25 +0400

    encapsulation, inheritance, polymorphism learning

---
+++ EIP/EIP.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+
+/*class vehicle
+{
+public:
+    // vehicle() : speed(), color() {}
+    vehicle(int a_speed, int a_color) : speed(a_speed), color(a_color)
+    {
+    }
+
+    void set_color(int a_color)
+    {
+        if (color >= 0 && color <= 255)
+        {
+            color = a_color;
+        }
+    }
+
+    void accelerate()
+    {
+        ++speed;
+    }
+
+    int get_color()
+    {
+        return color;
+    }
+
+    int get_speed()
+    {
+        return speed;
+    }
+
+protected:
+    int speed;
+private:
+    int color;
+};
+
+class motorcycle final : public vehicle
+{
+public:
+    motorcycle() : vehicle(0, 0), fuel()
+    {
+    }
+
+    motorcycle(int a_speed, int a_color, int a_fuel) : vehicle(a_speed, a_color), fuel(a_fuel)
+    {
+    }
+
+    void set_fuel(double a_fuel)
+    {
+        if (a_fuel > 0)
+        {
+            fuel = a_fuel;
+        }
+    }
+
+    double get_fuel()
+    {
+        return fuel;
+    }
+
+    void use_nitro()
+    {
+        speed *= 2;
+    }
+
+    void show()
+    {
+        std::cout << "color" << get_color() << '\n';
+        std::cout << "fuel" << get_fuel() << '\n';
+        std::cout << "speed" << get_speed() << '\n';
+    }
+
+private:
+    double fuel;
+};
+
+class bicycle
+{
+public:
+};*/
+
+
+int main(int argc, char* argv[])
+{
+    // motorcycle moto{4, 5, 10};
+    // moto.accelerate();
+    // moto.use_nitro();
+    // moto.show();
+}
